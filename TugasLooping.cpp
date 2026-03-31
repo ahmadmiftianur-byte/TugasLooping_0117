2934db77421b3c7f45ba3b47af32cb0bcf8405a5
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-03-31 22:21:20 +0700

    menambahkan while if

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -9,4 +9,12 @@ bool prima(int x) {
     int i = 2;
     if (x <= 1) return false;
 
-    
+    while (i < x) {
+        if (x % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
